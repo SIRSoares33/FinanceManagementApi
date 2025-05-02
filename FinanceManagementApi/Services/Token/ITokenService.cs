acf4f53b08a3bb15ba16095eb51d30a72392be56
SIRSoares33 <sirgustavo4@gmail.com>
2025-05-01 22:48:58 -0300

    refactor(ITokenService): Documentando a interface com summary

--- FinanceManagementApi/Services/Token/ITokenService.cs
+++ FinanceManagementApi/Services/Token/ITokenService.cs
@@ -2,8 +2,16 @@ using FinanceManagementApi.Models.User;
 
 namespace FinanceManagementApi.Services.Token
 {
+    /// <summary>
+    /// Interface para gerar tokens de autenticação
+    /// </summary>
     public interface ITokenService
     {
+        /// <summary>
+        /// Gera um token para authenticação
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
         string GenerateToken(UserModel user);
     }
 }
