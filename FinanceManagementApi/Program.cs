3e853a1f054865f93d877aca3f8780b254a485d6
Gustavo Soares <sirgustavo4@gmail.com>
2025-05-01 21:09:12 -0300

    refactor: Configurando a program

--- FinanceManagementApi/Program.cs
+++ FinanceManagementApi/Program.cs
@@ -1,3 +1,13 @@
+using System.Text;
+using FinanceManagementApi.Context;
+using FinanceManagementApi.Repository;
+using FinanceManagementApi.Repository.User;
+using FinanceManagementApi.Services.Token;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -10,9 +20,55 @@ builder.Services.AddSwaggerGen();
 #endregion
 
 #region Context
+builder.Services.AddDbContext<IContext, AppDbContext>(x => x.UseSqlite("DataSource=app.db"));
+#endregion
+
+#region Config
+// CORS configuration
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder => builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
+// ModelState error Configuration
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.InvalidModelStateResponseFactory = context =>
+    {
+        var primeiroErro = context.ModelState
+            .Values
+            .SelectMany(v => v.Errors)
+            .Select(e => e.ErrorMessage)
+            .FirstOrDefault();
+
+        return new UnprocessableEntityObjectResult(primeiroErro);
+    };
+});
+#endregion
+
+#region Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("JwtSecretKey")!))
+        };
+    });
 #endregion
 
 #region DI
+// Repository
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+// TokenService
+builder.Services.AddSingleton<ITokenService, TokenService>();
 #endregion
 
 var app = builder.Build();
