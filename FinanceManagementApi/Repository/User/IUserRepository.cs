3acc497713e7eb29b4b0bec0a50e83133ac2350f
SIRSoares33 <sirgustavo4@gmail.com>
2025-05-01 22:47:39 -0300

    refactor(UserRepository): Adicionando o método de registrar usuários

--- FinanceManagementApi/Repository/User/IUserRepository.cs
+++ FinanceManagementApi/Repository/User/IUserRepository.cs
@@ -5,6 +5,17 @@ namespace FinanceManagementApi.Repository.User
 {
     public interface IUserRepository
     {
+        /// <summary>
+        /// Realiza o login do usuário.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
         Task<UserModel?> Login(LoginModel model);
+        /// <summary>
+        /// Realiza o cadastro do usuário.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        Task RegisterAsync(UserModel model);
     }
 }
--- FinanceManagementApi/Repository/User/UserRepository.cs
+++ FinanceManagementApi/Repository/User/UserRepository.cs
@@ -8,7 +8,22 @@ namespace FinanceManagementApi.Repository
 {
     public class UserRepository(IContext context) : IUserRepository
     {
+        #region Methods
+        // IUserRepository
         public Task<UserModel?> Login(LoginModel model)
             => context.Users.FirstOrDefaultAsync(x => x.Email == model.Email && x.Password == model.Password);   
+            
+        public async Task RegisterAsync(UserModel model)
+        {
+            if (await VerifyEmailExistsInDb(model.Email))
+                throw new Exception("Email já existente.");
+
+            await context.Users.AddAsync(model);   
+            await context.Save();   
+        }
+        // Local
+        private Task<bool> VerifyEmailExistsInDb(string email)
+            => context.Users.AnyAsync(x => x.Email == email);
+        #endregion
     }
 }
