1cd6d6a43c0b46b3a19050dee13a5fc01d18b367
Gustavo Soares <sirgustavo4@gmail.com>
2025-05-06 12:57:06 -0300

    feat: Criando a TransactionRepository

---
+++ FinanceManagementApi/Repository/Transaction/TransactionRepository.cs
@@ -0,0 +1,32 @@
+using FinanceManagementApi.Context;
+using FinanceManagementApi.Models.Transaction;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceManagementApi.Repository.Transaction
+{
+    public class TransactionRepository(IContext context) : ITransactionRepository
+    {
+        #region Methods
+        // ITransactionRepository
+        public async Task AddTransactionAsync(TransactionModel transactionModel)
+            { await context.Transactions.AddAsync(transactionModel); await context.Save(); }
+        
+        public async Task DeleteTransactionAsync(int transactionId)
+            { context.Transactions.Remove(await GetTransactionByIdAsync(transactionId)); await context.Save(); }
+
+        public async Task UpdateTransactionAsync(int transactionId, TransactionModel transactionModel)
+        {
+            await DeleteTransactionAsync(transactionId);
+            
+            transactionModel.Id = transactionId;
+            
+            await context.Transactions.AddAsync(transactionModel);
+            await context.Save();
+        }
+
+        private async Task<TransactionModel> GetTransactionByIdAsync(int transactionId)
+            => await context.Transactions.FirstOrDefaultAsync(x => x.Id == transactionId) 
+                ?? throw new Exception("Transação não encontrada.");
+        #endregion
+    }
+}
