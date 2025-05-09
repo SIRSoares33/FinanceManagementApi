918dbf8db290b9ad52f03636b76c02fb22441263
Gustavo Soares <sirgustavo4@gmail.com>
2025-05-09 14:48:43 -0300

    feat: Criando o HttpService

---
+++ FinanceManagementBlazor/Services/HttpRequests/HttpService.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http.Headers;
+using FinanceManagementBlazor.Storage.Token;
+
+namespace FinanceManagementBlazor.Services.HttpRequests
+{
+    public class HttpService(ITokenStorage tokenStorage, HttpClient http) : IHttpService
+    {
+        #region IIHttpService Methods
+        public HttpClient GetHttpClient(bool withAuthHeader)
+        {
+            if (withAuthHeader)
+                http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenStorage.GetItem());
+            
+            return http;
+        }
+        #endregion
+    }
+}
+
+public interface IHttpService
+{
+    /// <summary>
+    /// Retorna uma instância de HtttpClient com ou sem autenticação.
+    /// </summary>
+    /// <param name="withAutgh"></param>
+    /// <returns></returns>
+    HttpClient GetHttpClient(bool withAutgh);
+}
